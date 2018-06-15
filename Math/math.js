383bdd70324bcc551d95909cafc54952295ae910
Henrique S. Matulis <henrique.matulis@gmail.com>
2018-06-15 17:20:34 -0400

    Fix problem distribution and change timing
    Now amount of time depends on problem answer and number of terms

--- Math/math.js
+++ Math/math.js
@@ -1,60 +1,77 @@
 /*jslint white: true, bitwise: true, eqeq: true, nomen: true, plusplus: true, vars: true, maxerr: 50*/
 
-
-function createAdd(result, operations){
-    /**
-    Asssumes result > operations
-    **/
-    "use strict";
-    if (!operations || operations <= 0){
-        return [result];
-    }
-    
-    if (result <= 0){
-        return [0, 0];
-    }
-    
-    var leftOps = Math.floor((operations - 1) / 2 ) ;
-    var rightOps = (operations - 1 - leftOps);
-    
-    var random = Math.round(Math.random() * result);
-    var rightValue = Math.max( Math.min(random, result - leftOps - 1), rightOps + 1);
-    var leftValue = result - rightValue;
-    
-    var left = createAdd(leftValue, leftOps);
-    var right = createAdd(rightValue, rightOps);
-    return  left.concat(right);
-}
-
-function format_add(strs, strict){
-    "use strict";
-    if(strs.length == 1){
-        return strs[0];
-    }
-    if(strs.length > 2){
-        return (strict?"(":"") + strs[0] + " + " + strs[1] + ' + ' + format_add(strs.slice(2), false) + (strict?")":"");
-    }
-    return (strict?"(":"") + strs[0] + " + " + strs[1] + (strict?")":"");
-    
-}
-
-
 // Global vars
 var timeLimit;
 var new_;
 var old;
-var timer;
+
+var timeLimitFunc;
+
 var start = new Date().getTime();
+var timer;
 
+window.onload = function() {
+   "use strict";
 
-
-
-function makeProblem(result){
-    "use strict";
-    var nums = createAdd(result, 2);
-    return format_add(nums, false);
+	new_ = document.getElementById('new');
+	old = document.getElementById('old');
+	timer = document.getElementById('timer');
     
-    
+	timeLimitFunc = (result, terms) => (45 * result + 2500 * terms);
+	
+	question();
+	setInterval(refreshTimer, 100);
+};
+
+
+var refreshTimer = function(){
+	"use strict";
+	var t = new Date().getTime();
+	timer.innerHTML = "Timer: "+Math.round((timeLimit - t + start) / 100) / 10;
+}
+
+
+// Standard Normal variate using Box-Muller transform.
+function randn_bm() {
+    var u = 0, v = 0;
+    while(u === 0) u = Math.random(); //Converting [0,1) to (0,1)
+    while(v === 0) v = Math.random();
+    return Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v );
+}
+
+
+function buildQuestion(number, question){
+		if(question == "" || typeof question !== "string"){
+			return "" + number;
+		}
+	
+		if(number >= 0){
+			return question + " + " + number;
+		} else {
+			return question + " - " + (-number);
+		}
+}
+
+function makeQuestion(result, amount){
+	let question = "";
+	let left = result;
+	let arr = [];
+	for(let i=0; i < amount - 1; i++){
+			let mean = left / (amount - i);
+			let std = mean / 2;
+			let number = Math.round(std * randn_bm() + mean);
+			left -= number;
+			arr.push(number);
+			question = buildQuestion(number, question);
+	}
+	
+	question = buildQuestion(left, question);
+	arr.push(left);
+	return [question, arr];
+}
+
+function write(question_text){
+	new_.innerHTML = "<p>" + (question_text)+ "</p>";
 }
 
 var yieldAnswer = function(equation, result){
@@ -62,44 +79,30 @@ var yieldAnswer = function(equation, result){
     old.innerHTML = "<p>" + (equation + " = " + result)+ "</p>" + old.innerHTML;
 }
 
-var newProblem = function(){
-	"use strict";
+function question() {
+   let ans = Math.floor(Math.random() * 101);
+	
+	let top;
+	if(ans > 60) {
+		top = 4;
+	} else if( ans > 20){
+		top = 3;
+	} else {
+		top = 2;
+	}
+	
+	
+	let amt = Math.floor(Math.random() * (top - 1)) + 2;
+	let r = makeQuestion(ans, amt);
+	
+    timeLimit = timeLimitFunc(ans, amt);
+	write(r[0]);
+	
 	start = new Date().getTime();
-    var result = Math.max(Math.round(Math.random() * 101), 6);
-	var equation = makeProblem(result);
-    new_.innerHTML = "<p>" + (equation + " = ???")+ "</p>";
-    setTimeout(function() {
-    yieldAnswer(equation, result);
-}, timeLimit);
-    
-}
-
-var refreshTimer = function(){
-	"use strict";
-	var t = new Date().getTime();
-	timer.innerHTML = "Timer: "+Math.round((timeLimit - t + start) / 100) / 10;
 	
-	
+	setTimeout( () => yieldAnswer(r[0], ans), timeLimit);
+	setTimeout(question, timeLimit);
+	refreshTimer();
 }
 
 
-
-
-
-
-
-
-window.onload = function() {
-   "use strict";
-
-
-	new_ = document.getElementById('new');
-	old = document.getElementById('old');
-	timer = document.getElementById('timer');
-    
-    timeLimit = 1000 * 5;
-	
-	newProblem();
-	setInterval(refreshTimer, 100);
-	setInterval(newProblem, timeLimit);
-};
